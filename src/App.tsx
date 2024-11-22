d781207da846feb96c156d31487c1ee890f01d72
21mad <ilmir.abyzov@mail.ru>
2024-11-23 01:59:46 +0300

    just a chill fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import WebApp from '@twa-dev/sdk';
+import { useRef, useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+// import './App.css';
+// import WebApp from '@twa-dev/sdk';
 
 function App() {
   // const [count, setCount] = useState(0);
